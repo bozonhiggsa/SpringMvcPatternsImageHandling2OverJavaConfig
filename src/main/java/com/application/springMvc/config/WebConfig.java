3243ddf86ebc332a3eb05504573208b180c01a78
bozonhiggsa <myrealjobandmoney>
2018-02-23 22:40:14 +0200

    Initial commit

---
+++ src/main/java/com/application/springMvc/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.application.springMvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Java Configuration
+ * @author Ihor Savchenko
+ * @version 1.0
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.application.springMvc.controller")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(15000000);
+        return new CommonsMultipartResolver();
+    }
+
+}
